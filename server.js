3fbe142a5ae4ac53d2d7982808dd17bbf9e0e9c9
Anthony Gonzales <anthonygonzales79072@gmail.com>
2021-11-09 13:10:42 -0600

    PORT donenv

--- server.js
+++ server.js
@@ -48,5 +48,7 @@ app.get("/about", (req, res) => {
 
 /////////////////////
 // Listener
-////////////////////
-app.listen(4000, () => console.log("Listening on port 4000"))
+///////////////////
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
